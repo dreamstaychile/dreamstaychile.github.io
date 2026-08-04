371374ae1b94323034ddeece1001f3ec7fa5f844
dreamstaychile <pablo.loyola@dreamstaychile.com>
2026-08-04 15:06:00 -0400

    Connect property form to Google Sheets

---
+++ administra-tu-propiedad/config.js
@@ -0,0 +1,4 @@
+/* El endpoint debe recibir POST JSON, validar y sanitizar en servidor, calcular el
+   scoring, guardar el lead y disparar notificaciones. Déjalo vacío hasta contar
+   con un servicio seguro; nunca incluyas secretos en este archivo público. */
+window.DREAM_STAY_LEADS_ENDPOINT = "https://script.google.com/macros/s/AKfycbzUY5UE13IUGzqWvZcyE5YFfk1Fl0pVz2K6XrpC6xKVRxgn0aAwGavOLg4udyio6itV/exec";
